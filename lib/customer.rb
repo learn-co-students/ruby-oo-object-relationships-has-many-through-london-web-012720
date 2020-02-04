99f101e794fb38fce7d5181168d6997f00c1a963
Amy Franz <amy.franz@gmail.com>
2020-02-04 12:27:08 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
 class Customer
-  
+    attr_reader :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def waiters
+        meals.map{|m| m.waiter}
+    end
+
+    def meals
+        Meal.all.select{|m| m.customer == self}
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,29 @@
 class Waiter
+    attr_reader :name, :age
 
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select{|m| m.waiter == self}
+    end
+
+    def best_tipper
+        #meals.max_by{|m| m.tip}.customer
+        meals.max_by(&:tip).customer
+    end
 end
